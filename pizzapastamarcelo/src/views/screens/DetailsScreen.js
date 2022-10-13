7997349f8f9e24ed17b601a8fe619f809358ff52
TheGrimReaper97 <gers.ch@hotmail.com>
2022-10-13 03:46:50 -0600

    test1

---
+++ pizzapastamarcelo/src/views/screens/DetailsScreen.js
@@ -0,0 +1,11 @@
+import { SafeAreaView, Text} from "react-native"
+
+const DetailsScreen = () => {
+    return (
+        <SafeAreaView>
+            <Text>Ejemplo</Text>
+        </SafeAreaView>
+    )
+}
+
+export default DetailsScreen;
