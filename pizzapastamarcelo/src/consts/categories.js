a4f1472c7c570b0de7ff00937c12a3a358fd8c51
TheGrimReaper97 <gers.ch@hotmail.com>
2022-11-02 15:20:21 -0600

    Expo2

--- pizzapastamarcelo/src/consts/categories.js
+++
@@ -1,8 +0,0 @@
-
-const categories = [
-  {id: '1', name: 'Pizza', image: require('../assets/categories/pizza.png')},
-  {id: '2', name: 'Pasta', image: require('../assets/categories/pasta.png')},
-  
-];
-
-export default categories;
